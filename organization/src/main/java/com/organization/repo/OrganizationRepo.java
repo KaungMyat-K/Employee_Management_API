82507d2e07bb328dc0f02c4a6c397a4363e9caaf
KaungMyat-K <kaungmyat.dev8@gmail.com>
2024-07-12 16:01:33 +0630

    add organization service

---
+++ organization/src/main/java/com/organization/repo/OrganizationRepo.java
@@ -0,0 +1,11 @@
+package com.organization.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.organization.entity.Organization;
+
+@Repository
+public interface OrganizationRepo extends JpaRepository<Organization,Long> {
+    
+}
